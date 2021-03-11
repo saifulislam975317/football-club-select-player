955288047b241398490b7cf430d6692c72bf4dc2
saifulislam975317 <saifulislam975317@gmail.com>
2021-03-11 23:11:34 +0600

    info

--- src/Components/ClubDetails/ClubDetails.js
+++ src/Components/ClubDetails/ClubDetails.js
@@ -44,13 +44,11 @@ const ClubDetails = (props) => {
             </div>
 
            <div className="social-icons">
-           <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
-           <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter-square"></i></a>
-           <a href="https://www.youtube.com" target="_blank"><i class="fab fa-youtube"></i></a>
+           <a href="https://www.facebook.com" target='_blank'><i class="fab fa-facebook"></i></a>
+           <a href="https://www.twitter.com" target='_blank'><i class="fab fa-twitter-square"></i></a>
+           <a href="https://www.youtube.com" target='_blank'><i class="fab fa-youtube"></i></a>
            
            
-           
-       
            </div>
         
         
